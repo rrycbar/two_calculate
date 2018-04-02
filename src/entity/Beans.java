565a3622640693e82ddd7609008c47c4e010040b
mafx8859 <2365923606@qq.com>
2018-04-02 15:59:11 +0800

    houTai

---
+++ src/entity/Beans.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Beans {
+    private String atith;
+    private int result;
+    public String getAtith() {
+	    return atith;
+    }
+	public void setAtith(String atith) {
+		this.atith = atith;
+	}
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+   
+}
